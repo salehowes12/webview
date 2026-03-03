9fdd19f29381c16075bd13fb5b6547d89384c5cd
salehowes12 <s12326654@stu.najah.edu>
2026-03-03 21:19:04 +0200

    Initial commit

--- app/webview.tsx
+++ app/webview.tsx
@@ -16,9 +16,11 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    width: '100%',
+    height: '100%',
   },
   webview: {
-    flex: 1,
+     width: '100%',
+    height: '100%',
   },
 });
